6a9609c71ca41a7a17f810aa48d8831f617c538d
JasonCleffry <95690165+JasonCleffry@users.noreply.github.com>
2021-12-07 04:33:12 -0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace Forms1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Font = new System.Drawing.Font("Georgia", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.button1.Location = new System.Drawing.Point(120, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.Font = new System.Drawing.Font("Gadugi", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.DarkCyan;
+            this.button2.Location = new System.Drawing.Point(720, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 70);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Roll!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label1.Location = new System.Drawing.Point(210, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(328, 59);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "\"4 Dices\" Game";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(550, 100);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(65, 65);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Location = new System.Drawing.Point(120, 120);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(750, 250);
+            this.panel1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Calibri", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.DarkOrange;
+            this.button3.Location = new System.Drawing.Point(420, 65);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 70);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Rules";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(0, 370);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1000, 200);
+            this.panel2.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(130, 100);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(65, 65);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(984, 562);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int[] num = new int[6] { 1, 2, 3, 4, 5, 6 };
+
+            Random rnd1 = new Random();
+            int index1 = rnd1.Next(num.Length);
+            int i = num[index1];
+            Console.WriteLine($"Value #1: {num[index1]}");
+
+            System.Threading.Thread.Sleep(200);
+            Random rnd2 = new Random();
+            int index2 = rnd2.Next(num.Length);
+            int j = num[index2];
+            Console.WriteLine($"Value #2: {num[index2]}");
+
+            System.Threading.Thread.Sleep(400);
+            Random rnd3 = new Random();
+            int index3 = rnd3.Next(num.Length);
+            int k = num[index3];
+            Console.WriteLine($"Value #3: {num[index3]}");
+
+            System.Threading.Thread.Sleep(300);
+            Random rnd4 = new Random();
+            int index4 = rnd4.Next(num.Length);
+            int l = num[index4];
+            Console.WriteLine($"Value #4: {num[index4]}");
+
+            MessageBox.Show($"Value #1: {num[index1]}");
+            MessageBox.Show($"Value #2: {num[index2]}");
+            MessageBox.Show($"Value #3: {num[index3]}");
+            Console.ReadLine();
+
+            int m;
+            m = (i + j + k);
+            int n;
+            n = (m + l);
+
+            Console.WriteLine("Ace Result: " + m);
+            Console.WriteLine("Final Result: " + n);
+            Console.ReadLine();
+
+            if (m > 11)
+            {
+                Console.ReadLine();
+                MessageBox.Show("Your Ace Result: " + m);
+            }
+            else
+            {
+                Console.ReadLine();
+                MessageBox.Show("Please try the fourth dice...");
+                MessageBox.Show($"Value #4: {num[index4]}");
+                MessageBox.Show("Your Final Result: " + n);
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Press 【Roll!】 to start. Or press 【Exit】 to exit the program.\n The rules of the game: Roll out three dice at one time and check the sum.\n If the sum is greater than or equal to 12, the game ends immediately.\n If the sum is less than 12, there is an additional chance to roll the fourth dice\n and add the result to the previous sum.\n After that, the game ends.");
+        }
+    }
+}
